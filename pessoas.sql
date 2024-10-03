e9cbad6bd011ce52f392f765340ee4070394db32
Glaudencio12 <glaudenciocosta015@gmail.com>
2024-10-03 18:13:42 -0300

    "Décimo terceiro commit"

--- pessoas.sql
+++ pessoas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27/09/2024 às 01:55
+-- Tempo de geração: 03/10/2024 às 23:11
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -45,11 +45,13 @@ CREATE TABLE `pessoas` (
 --
 
 INSERT INTO `pessoas` (`id`, `nome`, `cpf`, `sexo`, `data_nascimento`, `email`, `telefone`, `endereco`, `cidade`, `estado`) VALUES
-(1, 'Francisco das Chagas de Oliveira', '51239875564', 'Masculino', '2001-04-10', 'francisco@gmail.com', '11561116155', 'rua número 6', 'Teresina', 'PI'),
-(10, 'João Lucas', '1516186121518', 'Masculino', '2000-02-10', 'jooao2@gmail.com', '51151518112', 'Quiabos', 'Coelho neto', 'MA'),
-(13, 'Raimundo', '15541511515', 'Masculino', '2000-02-03', 'raimundo2@gmail.com', '(99) 99999-6525', 'Penera', 'Fortaleza', 'PE'),
-(14, 'André das chagas', '06656512697', 'Masculino', '1988-05-02', 'andre@gmail.com', '(99) 99999-6525', 'Avenida Manechal', 'caxias', 'RJ'),
-(17, 'Gabriel Sousa Silva', '985289225228', 'Outro', '2008-01-08', 'G@gmail.com', '9844951818', 'Centro', 'Coelho Neto', 'RN');
+(20, 'Miguel Costa', '51239875561', 'Masculino', '2002-08-20', 'miguel@gmail.com', '66548488484', 'Monte castelo', 'Fortaleza', 'PB'),
+(22, 'Jose Ribamar', '155415115155', 'Masculino', '1999-02-01', 'ribamar@gmail.com', '(99) 99999-6525', 'avenida calhau', 'São Luís', 'MA'),
+(23, 'Zé Rabelo', '51239875564', 'Masculino', '2024-09-10', 'glaude12@gmail.com', '(99)99999-9999', 'Parque Amazonas', 'Coelho Neto', 'PI'),
+(26, 'Chico Carlos', '06656512697', 'Masculino', '2006-05-20', 'franciasoliveiraso@gmail.com', '(99)99999-6525', 'Loteamento Bom Sucesso', 'caxias', 'PE'),
+(28, 'Maria', '08000654555', 'Feminino', '1999-04-20', 'franco@gmail.com', '(99)99999-6525', 'Curralinho da leonarda ', 'Duque Bacelar', 'MA'),
+(30, 'RAIMUNDO CORREIA BASTOS', '0665651269792', 'Masculino', '2006-01-10', 'fhagasoliveiraso@gmail.com', '(99)99999-9998', 'Cocal', 'Duque Bacelar', 'ES'),
+
 
 --
 -- Índices para tabelas despejadas
@@ -60,7 +62,8 @@ INSERT INTO `pessoas` (`id`, `nome`, `cpf`, `sexo`, `data_nascimento`, `email`, 
 --
 ALTER TABLE `pessoas`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `cpf` (`cpf`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -70,7 +73,7 @@ ALTER TABLE `pessoas`
 -- AUTO_INCREMENT de tabela `pessoas`
 --
 ALTER TABLE `pessoas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
